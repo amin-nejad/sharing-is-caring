5d80a402bd6da6a82d10645fc1f244d58f86fdef
Ali Amin-Nejad <aanejad@hotmail.co.uk>
2021-02-14 02:40:58 +0000

    update form + link firebase

--- functions/register.js
+++ functions/register.js
@@ -1,22 +1,45 @@
-const admin = require('firebase-admin')
-const serviceAccount = require('./serviceAccountKey.json')
+const firebase = require('firebase')
+const qs = require('querystring');
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://long-victor-304116-default-rtdb.europe-west1.firebasedatabase.app"
-})
+const firebaseConfig = {
+  apiKey: "AIzaSyCNGralWpRTUNJgPJdJLOYcZ4xXj-gxBC8",
+  authDomain: "long-victor-304116.firebaseapp.com",
+  databaseURL: "https://long-victor-304116-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "long-victor-304116",
+  storageBucket: "long-victor-304116.appspot.com",
+  messagingSenderId: "668790316116",
+  appId: "1:668790316116:web:f5a416eb1b98cf4f4de1c1"
+};
 
-const db = admin.firestore()
+exports.handler = async (event) => {
 
-exports.handler = async (event, context, callback) => {
-  await db.collection('COLLECTION').add({
-    name: 'Test'
-  })
 
-  return callback(null, {
-    statusCode: 200,
-    body: JSON.stringify({
-      data: `Test data added successfully`
-    })
-  })
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+  } else {
+    firebase.app();
+  }
+
+  console.log(event.body)
+  try {
+    const body = qs.parse(event.body.toString());
+    const {name, email, service, gmap} = body
+    return firebase.database().ref('registrations').push().set({name, email, service, gmap})
+      .then((result) => {
+        return {
+          statusCode: 200,
+          body: JSON.stringify({success: 'True'})
+        }})
+      .catch((err) => {
+        return {
+          statusCode: 500,
+          body: JSON.stringify({success: 'False', errorMessage: err})
+        }})
+  } catch (err) {
+    console.log(err)
+    return {
+      statusCode: 500,
+      body: JSON.stringify({success: 'False'})
+    }
+  }
 }
