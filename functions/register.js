d571336e162d728c6d3d48af4182e1c10799a421
Ali Amin-Nejad <aanejad@hotmail.co.uk>
2021-03-14 17:29:45 +0000

    add email confirmations to sign ups

--- functions/register.js
+++ functions/register.js
@@ -1,5 +1,8 @@
 const firebase = require('firebase')
 const qs = require('querystring');
+var nodemailer = require('nodemailer');
+const smtpURL = process.env.SMTP_URL;
+let transporter = nodemailer.createTransport(smtpURL)
 
 const firebaseConfig = {
   apiKey: "AIzaSyCNGralWpRTUNJgPJdJLOYcZ4xXj-gxBC8",
@@ -26,6 +29,29 @@ exports.handler = async (event) => {
     const {name, email, businessName, service, gmapWebsite, gmapLat, gmapLong, gmapURL, gmapPlaceID} = body
     const datetime =  new Date().toLocaleString('en-GB', { timeZone: 'UTC' })
     return firebase.database().ref('registrations').push().set({datetime, name, email, businessName, service, gmapWebsite, gmapLat, gmapLong, gmapURL, gmapPlaceID, verified: "N"})
+      .then((result) => {
+        var mailOptions = {
+          from: 'Andreas Kattou <noreply@sharingiscaring.charity>',
+          to: email,
+          bcc: 'aanejad@hotmail.co.uk',
+          subject: 'Sharing is Caring Registration Confirmation',
+          html: `<p>Hi ${name},</p> 
+          <p>Thank you for signing up <a href="${gmapURL}">${businessName}</a> to Sharing is Caring, Serve our Nation!</p>
+          <p>Best of luck and don't forget to follow our instructions on displaying the logo and promoting your scheme.</p>
+          <p>Kind regards,<br>Andreas</p>
+          <p>NB. This mailbox is not monitored. If you need to get in touch, contact us on <a href="mailto:hello@sharingiscaring.charity">hello@sharingiscaring.charity</a></p>`,
+        };
+        
+        transporter.sendMail(mailOptions, function(error, info){
+          if (error) {
+            console.log(error);
+            return error
+          } else {
+            console.log('Email sent: ' + info.response);
+            return info.response
+          }
+        });  
+      })
       .then((result) => {
         return {
           statusCode: 200,
